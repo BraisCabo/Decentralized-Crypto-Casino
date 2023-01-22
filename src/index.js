c1e6bc6c2eed8eb0ee5c2e36a2223ac19fc215fe
Brais <braiscabofelpete@gmail.com>
2023-01-22 01:25:50 +0100

    Last

--- src/index.js
+++ src/index.js
@@ -1,31 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux'
-import './index.css';
-import App from './App';
-import {
-  BrowserRouter as Router,
-} from "react-router-dom"
-import 'core-js/stable/index.js'
-import 'regenerator-runtime/runtime.js'
-import store from './store'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import "core-js/stable/index.js";
+import "regenerator-runtime/runtime.js";
+import store from "./store";
 
-class Doc extends React.Component{
-  componentDidMount(){
-    document.title = "Cripto Casino"
-  }
-
-  render(){
-    return(
-      <b> test </b>
-    )
-  }
-}
-
-
-ReactDOM.createRoot(document.getElementById('root')).render
-(
-<Router>
-<Doc/>
-  <Provider store={store}><App /></Provider>
-  </Router>)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>
+);
